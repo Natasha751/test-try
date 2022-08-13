b83274c39495809952c9bc7d9af024072002ac97
Natasha751 <ideveloper93@gmail.com>
2022-08-13 07:54:22 +0530

    okie

---
+++ src/Component/Fc.js
@@ -0,0 +1,4 @@
+function Fc(){
+    return<div><h1>Hello good morning</h1></div>
+}
+export default Fc
---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Navbar extends React.Component {
+    render() {
+        return <nav className="navbar navbar-dark bg-dark">
+            <a className="navbar-brand" href="/home">Reading Data</a>
+        </nav>
+    }
+}
+
+export default Navbar
